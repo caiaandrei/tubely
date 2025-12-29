f8cbd8b47a6b6dbd3954914c8621c7181b41f0f1
caiaandrei14 <caiaandrei14@hotmail.com>
2025-12-29 23:20:27 +0200

    use video aspect ratio category

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -83,10 +86,17 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
 		return
 	}
+
+	ration, err := getVideoAspectRation(fileTemp.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
+		return
+	}
+
 	key := make([]byte, 32)
 	rand.Read(key)
 
-	randFileName := base64.URLEncoding.EncodeToString(key) + ".mp4"
+	randFileName := ration + "/" + base64.URLEncoding.EncodeToString(key) + ".mp4"
 
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
@@ -113,4 +123,53 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong", err)
 		return
 	}
+}
+
+func getVideoAspectRation(path string) (string, error) {
+	execCom := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", path)
+	var out bytes.Buffer
+	execCom.Stdout = &out
+	err := execCom.Run()
+	if err != nil {
+		return "", err
+	}
+
+	type stream struct {
+		Width  int
+		Height int
+	}
+
+	type streams struct {
+		Streams []stream
+	}
+	var jsonRes streams
+	err = json.Unmarshal(out.Bytes(), &jsonRes)
+	if err != nil {
+		return "", err
+	}
+
+	width := jsonRes.Streams[0].Width
+	height := jsonRes.Streams[0].Height
+
+	//calculate this just because the exercise asked it...
+	gcdRes := gcd(jsonRes.Streams[0].Width, jsonRes.Streams[0].Height)
+
+	ratioWidth := width / gcdRes
+	rationHeight := height / gcdRes
+
+	if ratioWidth < rationHeight {
+		return "portrait", nil
+	} else if ratioWidth > rationHeight {
+		return "landscape", nil
+	} else {
+		return "other", nil
+	}
+}
+
+func gcd(nr1, nr2 int) int {
+	for nr2 != 0 {
+		nr1, nr2 = nr2, nr1%nr2
+	}
+
+	return nr1
 }
